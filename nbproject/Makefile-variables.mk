a1481158eb57d1673c8ac844c3653a0817fcee8f
bigz94 <lucien.zuercher@hotmail.com>
2012-07-22 20:03:11 +0200

    new start

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW_1-Windows
+CND_ARTIFACT_NAME_Debug=meteoritewars
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW_1-Windows/meteoritewars
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW_1-Windows/package
+CND_PACKAGE_NAME_Debug=meteoritewars.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW_1-Windows/package/meteoritewars.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW_1-Windows
+CND_ARTIFACT_NAME_Release=meteoritewars
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW_1-Windows/meteoritewars
+CND_PACKAGE_DIR_Release=dist/Release/MinGW_1-Windows/package
+CND_PACKAGE_NAME_Release=meteoritewars.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW_1-Windows/package/meteoritewars.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
