71b803877de2b9484a0974a62e2470437ab0c18f
Mogria <m0gr14@gmail.com>
2012-07-23 23:07:04 +0200

    added some comments to the methods of the world class

--- src/world.cpp
+++ src/world.cpp
@@ -3,16 +3,23 @@
 World::World()  {
   this->num_scenes = 0;
   this->current_scene = -1;
+
+  /* create pointer  */
   this->scenes = new Scene*[this->num_scenes];
 }
 
 World::~World() {
+  /* delete all the scenes from the memeory */
   for(int i = 0; i < this->num_scenes; i++) {
     delete this->scenes[i];
   }
+
+  /* free the pointer array */
   delete [] this->scenes;
 }
 
+/*  get the index of the scene which
+    has the given name */
 int World::get_scene_id(string &name) {
   int selected = -1;
   /* iterate the scenes and search for the given name */
@@ -24,6 +31,7 @@ int World::get_scene_id(string &name) {
   return selected;
 }
 
+/* get the pointer to the scene object */
 Scene *World::get_scene_by_id(int id) {
   Scene *selected = NULL;
   /* check if valid index */
@@ -33,14 +41,19 @@ Scene *World::get_scene_by_id(int id) {
   return selected;
 }
 
+/* get the pointer to the scene object
+   which has the given name*/
 Scene *World::get_scene(string &name) {
   return get_scene_by_id(get_scene_id(name));
 }
 
+/* get the currently loaded scene */
 Scene *World::get_current_scene() {
   return get_scene_by_id(this->current_scene);
 }
 
+/* add a scene object to the array of
+   scenes in this world */
 void World::add_scene(Scene *scene) {
   /* we need an array to temporary store the pointers
      to the scene objects, because we need to
@@ -52,9 +65,8 @@ void World::add_scene(Scene *scene) {
     tmp[i] = this->scenes[i];
   }
   
-  delete [] this->scenes;
-  
   /* reallocate memory */
+  delete [] this->scenes;
   this->scenes = new Scene*[this->num_scenes + 1];
 
   /* copy the values back  */
@@ -70,6 +82,11 @@ void World::add_scene(Scene *scene) {
   this->scenes[++this->num_scenes] = scene;
 }
 
+/* load the scene which has the given name
+
+   the on_load() method in the Scene object gets called
+   if an other scene was loaded before, the
+   on_unload()-method gets called too */
 void World::load_scene(string &name) {
   int id;
   Scene *current;
@@ -86,6 +103,11 @@ void World::load_scene(string &name) {
   current->on_load();
 }
 
+/* this unloads the current loaded scene
+   (if there is one)
+   
+   this also calls the unload_scene()-method
+   on the scene object */
 void World::unload_scene() {
   /* unload the old scene */
   Scene *current = get_current_scene();
