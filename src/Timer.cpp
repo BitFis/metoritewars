417d68cfdb53b7e1e943ebdd011aa0bdbf26d2c9
bigz94 <lucien.zuercher@hotmail.com>
2012-08-08 17:01:03 +0200

    added time class, todo

---
+++ src/Timer.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   Timer.cpp
+ * Author: zurcherl
+ * 
+ * Created on 8. August 2012, 16:58
+ */
+
+#include "Timer.h"
+
+Timer::Timer() {
+}
+
+Timer::Timer(const Timer& orig) {
+}
+
+timespec get(double time){
+  
+}
+
+timespec set(double time){
+  
+}
+
+double get(){
+  return 0.1223;
+}
+
+set(double time){
+  
+}
+
+Timer::~Timer() {
+}
+
--- src/engine/Object.h
+++ src/engine/Object.h
@@ -8,23 +8,6 @@
 #ifndef OBJECT_H
 #define	OBJECT_H
 
-<<<<<<< HEAD:src/Object.h
-#include "utility/Model_3DS.h"
-=======
-#include <iostream> //get helpful function and classes
-#include <GL/freeglut.h>
-#include <fstream>
-#include <string>
-#include <math.h>
-#include <GL/glext.h>
-
-#include "GLTexture.h"
->>>>>>> 60e1f97db0d97ee60e84c42128db1004799b9c74:src/engine/Object.h
-
-#define OBJECT_MAX_POINTS_PER_VERTEX 4
-#define OBJECT_TOTAL_FLOATS_IN_TRIANGLE 9
-#define OBJECT_TOTAL_FLOATS_IN_RECTANGLE 12
-
 using namespace std;
 
 class Object {
