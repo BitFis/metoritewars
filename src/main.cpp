5050a41b9ff603119f4fe1faccc6bab5bb0cedda
bigz94 <lucien.zuercher@hotmail.com>
2012-08-13 16:52:10 +0200

    tut 4 of irrlicht

--- src/main.cpp
+++ src/main.cpp
@@ -1,234 +1,255 @@
-/** Example 001 HelloWorld
+/** Example 004 Movement
 
-This Tutorial shows how to set up the IDE for using the Irrlicht Engine and how
-to write a simple HelloWorld program with it. The program will show how to use
-the basics of the VideoDriver, the GUIEnvironment, and the SceneManager.
-Microsoft Visual Studio is used as an IDE, but you will also be able to
-understand everything if you are using a different one or even another
-operating system than windows.
+This Tutorial shows how to move and animate SceneNodes. The
+basic concept of SceneNodeAnimators is shown as well as manual
+movement of nodes using the keyboard.  We'll demonstrate framerate
+independent movement, which means moving by an amount dependent
+on the duration of the last run of the Irrlicht loop.
 
-You have to include the header file <irrlicht.h> in order to use the engine. The
-header file can be found in the Irrlicht Engine SDK directory \c include. To let
-the compiler find this header file, the directory where it is located has to be
-specified. This is different for every IDE and compiler you use. Let's explain
-shortly how to do this in Microsoft Visual Studio:
+Example 19.MouseAndJoystick shows how to handle those kinds of input.
 
-- If you use Version 6.0, select the Menu Extras -> Options.
-  Select the directories tab, and select the 'Include' Item in the combo box.
-  Add the \c include directory of the irrlicht engine folder to the list of
-  directories. Now the compiler will find the Irrlicht.h header file. We also
-  need the irrlicht.lib to be found, so stay in that dialog, select 'Libraries'
-  in the combo box and add the \c lib/VisualStudio directory.
-  \image html "vc6optionsdir.jpg"
-  \image latex "vc6optionsdir.jpg"
-  \image html "vc6include.jpg"
-  \image latex "vc6include.jpg"
-
-- If your IDE is Visual Studio .NET, select Tools -> Options.
-  Select the projects entry and then select VC++ directories. Select 'show
-  directories for include files' in the combo box, and add the \c include
-  directory of the irrlicht engine folder to the list of directories. Now the
-  compiler will find the Irrlicht.h header file. We also need the irrlicht.lib
-  to be found, so stay in that dialog, select 'show directories for Library
-  files' and add the \c lib/VisualStudio directory.
-  \image html "vcnetinclude.jpg"
-  \image latex "vcnetinclude.jpg"
-
-That's it. With your IDE set up like this, you will now be able to develop
-applications with the Irrlicht Engine.
-
-Lets start!
-
-After we have set up the IDE, the compiler will know where to find the Irrlicht
-Engine header files so we can include it now in our code.
+As always, I include the header files, use the irr namespace,
+and tell the linker to link with the .lib file.
 */
+#ifdef _MSC_VER
+// We'll also define this to stop MSVC complaining about sprintf().
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(lib, "Irrlicht.lib")
+#endif
+
 #include <irrlicht/irrlicht.h>
 
-/*
-In the Irrlicht Engine, everything can be found in the namespace 'irr'. So if
-you want to use a class of the engine, you have to write irr:: before the name
-of the class. For example to use the IrrlichtDevice write: irr::IrrlichtDevice.
-To get rid of the irr:: in front of the name of every class, we tell the
-compiler that we use that namespace from now on, and we will not have to write
-irr:: anymore.
-*/
 using namespace irr;
 
 /*
-There are 5 sub namespaces in the Irrlicht Engine. Take a look at them, you can
-read a detailed description of them in the documentation by clicking on the top
-menu item 'Namespace List' or by using this link:
-http://irrlicht.sourceforge.net/docu/namespaces.html
-Like the irr namespace, we do not want these 5 sub namespaces now, to keep this
-example simple. Hence, we tell the compiler again that we do not want always to
-write their names.
+To receive events like mouse and keyboard input, or GUI events like "the OK
+button has been clicked", we need an object which is derived from the
+irr::IEventReceiver object. There is only one method to override:
+irr::IEventReceiver::OnEvent(). This method will be called by the engine once
+when an event happens. What we really want to know is whether a key is being
+held down, and so we will remember the current state of each key.
 */
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
+class MyEventReceiver : public IEventReceiver
+{
+public:
+	// This is the one method that we have to implement
+	virtual bool OnEvent(const SEvent& event)
+	{
+		// Remember whether each key is down or up
+		if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+
+		return false;
+	}
+
+	// This is used to check whether a key is being held down
+	virtual bool IsKeyDown(EKEY_CODE keyCode) const
+	{
+		return KeyIsDown[keyCode];
+	}
+	
+	MyEventReceiver()
+	{
+		for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+			KeyIsDown[i] = false;
+	}
+
+private:
+	// We use this array to store the current state of each key
+	bool KeyIsDown[KEY_KEY_CODES_COUNT];
+};
+
 
 /*
-To be able to use the Irrlicht.DLL file, we need to link with the Irrlicht.lib.
-We could set this option in the project settings, but to make it easy, we use a
-pragma comment lib for VisualStudio. On Windows platforms, we have to get rid
-of the console window, which pops up when starting a program with main(). This
-is done by the second pragma. We could also use the WinMain method, though
-losing platform independence then.
-*/
-
-
-/*
-This is the main method. We can now use main() on every platform.
+The event receiver for keeping the pressed keys is ready, the actual responses
+will be made inside the render loop, right before drawing the scene. So lets
+just create an irr::IrrlichtDevice and the scene node we want to move. We also
+create some other additional scene nodes, to show that there are also some
+different possibilities to move and animate scene nodes.
 */
 int main()
 {
-	/*
-	The most important function of the engine is the createDevice()
-	function. The IrrlichtDevice is created by it, which is the root
-	object for doing anything with the engine. createDevice() has 7
-	parameters:
 
-	- deviceType: Type of the device. This can currently be the Null-device,
-	   one of the two software renderers, D3D8, D3D9, or OpenGL. In this
-	   example we use EDT_SOFTWARE, but to try out, you might want to
-	   change it to EDT_BURNINGSVIDEO, EDT_NULL, EDT_DIRECT3D8,
-	   EDT_DIRECT3D9, or EDT_OPENGL.
+	// create device
+	MyEventReceiver receiver;
 
-	- windowSize: Size of the Window or screen in FullScreenMode to be
-	   created. In this example we use 640x480.
+	IrrlichtDevice* device = createDevice(video::EDT_OPENGL,
+			core::dimension2d<u32>(640, 480), 16, false, false, false, &receiver);
+	if (device == 0)
+		return 1; // could not create selected driver.
 
-	- bits: Amount of color bits per pixel. This should be 16 or 32. The
-	   parameter is often ignored when running in windowed mode.
-
-	- fullscreen: Specifies if we want the device to run in fullscreen mode
-	   or not.
-
-	- stencilbuffer: Specifies if we want to use the stencil buffer (for
-	   drawing shadows).
-
-	- vsync: Specifies if we want to have vsync enabled, this is only useful
-	   in fullscreen mode.
-
-	- eventReceiver: An object to receive events. We do not want to use this
-	   parameter here, and set it to 0.
-
-	Always check the return value to cope with unsupported drivers,
-	dimensions, etc.
-	*/
-	IrrlichtDevice *device =
-		createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 32,
-			false, false, false, 0);
-
-	if (!device)
-		return 1;
+	video::IVideoDriver* driver = device->getVideoDriver();
+	scene::ISceneManager* smgr = device->getSceneManager();
 
 	/*
-	Set the caption of the window to some nice text. Note that there is an
-	'L' in front of the string. The Irrlicht Engine uses wide character
-	strings when displaying text.
+	Create the node which will be moved with the WSAD keys. We create a
+	sphere node, which is a built-in geometry primitive. We place the node
+	at (0,0,30) and assign a texture to it to let it look a little bit more
+	interesting. Because we have no dynamic lights in this scene we disable
+	lighting for each model (otherwise the models would be black).
 	*/
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
-  
-	/*
-	Get a pointer to the VideoDriver, the SceneManager and the graphical
-	user interface environment, so that we do not always have to write
-	device->getVideoDriver(), device->getSceneManager(), or
-	device->getGUIEnvironment().
-	*/
-	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	IGUIEnvironment* guienv = device->getGUIEnvironment();
-
-	/*
-	We add a hello world label to the window, using the GUI environment.
-	The text is placed at the position (10,10) as top left corner and
-	(260,22) as lower right corner.
-	*/
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10,10,560,222), true);
-
-	/*
-	To show something interesting, we load a Quake 2 model and display it.
-	We only have to get the Mesh from the Scene Manager with getMesh() and add
-	a SceneNode to display the mesh with addAnimatedMeshSceneNode(). We
-	check the return value of getMesh() to become aware of loading problems
-	and other errors.
-
-	Instead of writing the filename sydney.md2, it would also be possible
-	to load a Maya object file (.obj), a complete Quake3 map (.bsp) or any
-	other supported file format. By the way, that cool Quake 2 model
-	called sydney was modelled by Brian Collins.
-	*/
-	IAnimatedMesh* mesh = smgr->getMesh("C:/irrlicht-1.7.3/media/sydney.md2");
-	if (!mesh)
-	{
-		device->drop();
-		return 1;
-	}
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
-
-	/*
-	To let the mesh look a little bit nicer, we change its material. We
-	disable lighting because we do not have a dynamic light in here, and
-	the mesh would be totally black otherwise. Then we set the frame loop,
-	such that the predefined STAND animation is used. And last, we apply a
-	texture to the mesh. Without it the mesh would be drawn using only a
-	color.
-	*/
+	scene::ISceneNode * node = smgr->addSphereSceneNode();
 	if (node)
 	{
-		node->setMaterialFlag(EMF_LIGHTING, false);
-		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture( 0, driver->getTexture("C:/irrlicht-1.7.3/media/sydney.bmp") );
+		node->setPosition(core::vector3df(0,0,30));
+		node->setMaterialTexture(0, driver->getTexture("media/wall.bmp"));
+		node->setMaterialFlag(video::EMF_LIGHTING, false);
 	}
 
 	/*
-	To look at the mesh, we place a camera into 3d space at the position
-	(0, 30, -40). The camera looks from there to (0,5,0), which is
-	approximately the place where our md2 model is.
+	Now we create another node, movable using a scene node animator. Scene
+	node animators modify scene nodes and can be attached to any scene node
+	like mesh scene nodes, billboards, lights and even camera scene nodes.
+	Scene node animators are not only able to modify the position of a
+	scene node, they can also animate the textures of an object for
+	example. We create a cube scene node and attach a 'fly circle' scene
+	node animator to it, letting this node fly around our sphere scene node.
 	*/
-	smgr->addCameraSceneNode(0, vector3df(0,30,-100), vector3df(0,5,0));
-  
+	scene::ISceneNode* n = smgr->addCubeSceneNode();
+
+	if (n)
+	{
+		n->setMaterialTexture(0, driver->getTexture("media/t351sml.jpg"));
+		n->setMaterialFlag(video::EMF_LIGHTING, false);
+		scene::ISceneNodeAnimator* anim =
+			smgr->createFlyCircleAnimator(core::vector3df(0,0,30), 20.0f);
+		if (anim)
+		{
+			n->addAnimator(anim);
+			anim->drop();
+		}
+	}
+
 	/*
-	Ok, now we have set up the scene, lets draw everything: We run the
-	device in a while() loop, until the device does not want to run any
-	more. This would be when the user closes the window or presses ALT+F4
-	(or whatever keycode closes a window).
+	The last scene node we add to show possibilities of scene node animators is
+	a b3d model, which uses a 'fly straight' animator to run between to points.
 	*/
+	scene::IAnimatedMeshSceneNode* anms =
+		smgr->addAnimatedMeshSceneNode(smgr->getMesh("media/ninja.b3d"));
+
+	if (anms)
+	{
+		scene::ISceneNodeAnimator* anim =
+			smgr->createFlyStraightAnimator(core::vector3df(100,0,60),
+			core::vector3df(-100,0,60), 3500, true, true);
+		if (anim)
+		{
+			anms->addAnimator(anim);
+			anim->drop();
+		}
+
+		/*
+		To make the model look right we disable lighting, set the
+		frames between which the animation should loop, rotate the
+		model around 180 degrees, and adjust the animation speed and
+		the texture. To set the right animation (frames and speed), we
+		would also be able to just call
+		"anms->setMD2Animation(scene::EMAT_RUN)" for the 'run'
+		animation instead of "setFrameLoop" and "setAnimationSpeed",
+		but this only works with MD2 animations, and so you know how to
+		start other animations. But a good advice is to not use
+		hardcoded frame-numbers...
+		*/
+		anms->setMaterialFlag(video::EMF_LIGHTING, false);
+
+		anms->setFrameLoop(0, 13);
+		anms->setAnimationSpeed(15);
+//		anms->setMD2Animation(scene::EMAT_RUN);
+
+		anms->setScale(core::vector3df(2.f,2.f,2.f));
+		anms->setRotation(core::vector3df(0,-90,0));
+//		anms->setMaterialTexture(0, driver->getTexture("media/sydney.bmp"));
+
+	}
+
+
+	/*
+	To be able to look at and move around in this scene, we create a first
+	person shooter style camera and make the mouse cursor invisible.
+	*/
+	smgr->addCameraSceneNodeFPS();
+	device->getCursorControl()->setVisible(false);
+
+	/*
+	Add a colorful irrlicht logo
+	*/
+	device->getGUIEnvironment()->addImage(
+		driver->getTexture("media/irrlichtlogoalpha2.tga"),
+		core::position2d<s32>(10,20));
+
+	gui::IGUIStaticText* diagnostics = device->getGUIEnvironment()->addStaticText(
+		L"", core::rect<s32>(10, 10, 400, 20));
+	diagnostics->setOverrideColor(video::SColor(255, 255, 255, 0));
+
+	/*
+	We have done everything, so lets draw it. We also write the current
+	frames per second and the name of the driver to the caption of the
+	window.
+	*/
+	int lastFPS = -1;
+
+	// In order to do framerate independent movement, we have to know
+	// how long it was since the last frame
+	u32 then = device->getTimer()->getTime();
+
+	// This is the movemen speed in units per second.
+	const f32 MOVEMENT_SPEED = 5.f;
+
 	while(device->run())
 	{
-		/*
-		Anything can be drawn between a beginScene() and an endScene()
-		call. The beginScene() call clears the screen with a color and
-		the depth buffer, if desired. Then we let the Scene Manager and
-		the GUI Environment draw their content. With the endScene()
-		call everything is presented on the screen.
-		*/
-		driver->beginScene(true, true, SColor(255,100,101,140));
+		// Work out a frame delta time.
+		const u32 now = device->getTimer()->getTime();
+		const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
+		then = now;
 
-		guienv->drawAll();
-		smgr->drawAll();
+		/* Check if keys W, S, A or D are being held down, and move the
+		sphere node around respectively. */
+		core::vector3df nodePosition = node->getPosition();
+
+    if(receiver.IsKeyDown(irr::KEY_ESCAPE))
+      device->closeDevice();
+    
+		if(receiver.IsKeyDown(irr::KEY_KEY_W))
+			nodePosition.Y += MOVEMENT_SPEED * frameDeltaTime;
+		else if(receiver.IsKeyDown(irr::KEY_KEY_S))
+			nodePosition.Y -= MOVEMENT_SPEED * frameDeltaTime;
+
+		if(receiver.IsKeyDown(irr::KEY_KEY_A))
+			nodePosition.X -= MOVEMENT_SPEED * frameDeltaTime;
+		else if(receiver.IsKeyDown(irr::KEY_KEY_D))
+			nodePosition.X += MOVEMENT_SPEED * frameDeltaTime;
+
+		node->setPosition(nodePosition);
+
+		driver->beginScene(true, true, video::SColor(255,113,113,133));
+
+		smgr->drawAll(); // draw the 3d scene
+		device->getGUIEnvironment()->drawAll(); // draw the gui environment (the logo)
 
 		driver->endScene();
-    
-    device->closeDevice();
+
+		int fps = driver->getFPS();
+
+		if (lastFPS != fps)
+		{
+			core::stringw tmp(L"Movement Example - Irrlicht Engine [");
+			tmp += driver->getName();
+			tmp += L"] fps: ";
+			tmp += fps;
+
+			device->setWindowCaption(tmp.c_str());
+			lastFPS = fps;
+		}
 	}
 
 	/*
-	After we are done with the render loop, we have to delete the Irrlicht
-	Device created before with createDevice(). In the Irrlicht Engine, you
-	have to delete all objects you created with a method or function which
-	starts with 'create'. The object is simply deleted by calling ->drop().
-	See the documentation at irr::IReferenceCounted::drop() for more
-	information.
+	In the end, delete the Irrlicht device.
 	*/
 	device->drop();
-
+	
 	return 0;
 }
 
 /*
-That's it. Compile and run.
+That's it. Compile and play around with the program.
 **/
